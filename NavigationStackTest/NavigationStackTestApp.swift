2c3d229e417e18172e0d476bd7745c79c7666aca
John Deranian <fun@scooby.cc>
2022-09-26 20:21:22 -0500

    Initial Commit

---
+++ NavigationStackTest/NavigationStackTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackTestApp.swift
+//  NavigationStackTest
+//
+//  Created by John Deranian on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
